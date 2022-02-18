32102e9bdded81d51952935508a3f409bdc512a2
basundhara <basundharasaha12398@gmail.com>
2022-02-18 09:36:24 +0530

    About us
    about us page and pics

---
+++ client/src/UI/Test.js
@@ -0,0 +1,13 @@
+const Test=(props)=>{
+    return(
+        <div>
+        <div><h1>oktest</h1></div>
+        <div><h1>oktest</h1></div>
+        <div><h1>oktest</h1></div>
+        <div><h1>oktest</h1></div>
+        </div>
+    );
+
+}
+
+export default Test;
